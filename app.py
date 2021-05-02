25f2c4ec8adf45c81c0b3e3f5c93607e88340073
Fantasyverse <81157390+Fantasyverse@users.noreply.github.com>
2021-05-02 16:35:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,81 @@
-from flask import Flask,request
-import tronapi
-from tronapi import Tron
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
-full_node = 'https://api.trongrid.io'
-solidity_node = 'https://api.trongrid.io'
-event_server = 'https://api.trongrid.io'
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! i am copy cat bot')
 
-PK = "af1bc382b5cffab7f2001ff0cbd9eab07193380bdf135881fe608eeda27ac1e1"
 
-tron = Tron(full_node=full_node,
-    solidity_node=solidity_node,
-    event_server=event_server)
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
 
-def setTronPK(pk):
-    tron.private_key = pk
-    tron.default_address = tron.address.from_private_key(pk).base58
 
-setTronPK(PK)
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
 
-app = Flask(__name__)
 
-def myfunc(add):
-  txn = tron.trx.send_token(PA, 10*100000*6, "1003134");
-  return "ok"
- 
-app.route('/')
-def getHandler():
-    return "ok"
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-@app.route('/post', methods = ['POST'])
-def getHandler():
-     r = request.json
-     PA = r["address"]
-     PS = r["amount"]
-     PR = r["tokenid"]
-     txn = tron.trx.send_token(PA, 10*100000*PS, PR);
-     return PK
-    
- 
-   
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1786486072:AAEPSkTKzuvWkskKctj_4wZy2teBQ08RjsQ", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
 if __name__ == '__main__':
- app.run()
+    main()
